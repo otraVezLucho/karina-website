6e3237c42b2572a7ea6b50d27690e8221020e551
otraVezLucho <otrodeluisvillada@gmail.com>
2026-02-27 16:07:38 -0500

    se soluciona navbar

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
+
+const menu_button = document.querySelector('.bx-menu');
+
+const close_button = document.querySelector('.close-menu');
+
+close_button.addEventListener('click', () => {
+    const ul_navbar = document.querySelector('.list-ul');
+    ul_navbar.classList.toggle('list-ul')
+});
+
+menu_button.addEventListener('click', () => {
+    const ul_navbar = document.querySelector('.list-ul-none');
+    ul_navbar.classList.toggle('list-ul');
+});
+
+
+
+
+
 const navbar = document.querySelector('.navbar');
 
 const body = document.querySelector('body');
